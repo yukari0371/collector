3b982c740bab7c3320fb6d009c63c46ab60d9c72
yukari4545 <tsurumaki0397@icloud.com>
2025-03-13 18:41:31 +0900

    Update urlSearch.ts

--- src/utils/urlSearch.ts
+++ src/utils/urlSearch.ts
@@ -1,3 +1,4 @@
+import fs from "fs";
 import * as cheerio from "cheerio";
 
 /** Types */
@@ -6,32 +7,38 @@ import {
 } from "../types/collector"
 
 export async function urlSearch(response: any): Promise<urlSearchResult> {
-    const urls: string[] = [];
+    return new Promise(async (resolve) => {
+        const urls: string[] = [];
 
-    try {
-        const $ = cheerio.load(response);
+        try {
+            const $ = cheerio.load(response);
+    
+            const links = $("a").map((_, element) => $(element).attr("href")).get();
+            const validUrls = links.filter(link => link.startsWith("https://") || link.startsWith("http://"));
+    
+            if (validUrls.length === 0) {
+                return resolve({
+                    status: "error",
+                    message: "Could not find url."
+                });
+            }
+            
+            validUrls.map(url => {
+                if (urls.includes(url)) return;
+                urls.push(url);
+            });
 
-        const links = $("a").map((_, element) => $(element).attr("href")).get();
-        const validUrls = links.filter(link => link.startsWith("https://") || link.startsWith("http://"));
-
-        if (validUrls.length === 0) {
-            return {
-                status: "error",
-                message: "Could not find url."
-            };
+        } catch (e) {
+            if (e instanceof Error) {
+                return resolve({
+                    status: "error",
+                    message: e.message
+                });
+            }
         }
-        
-        urls.push(...validUrls);
-    } catch (e) {
-        if (e instanceof Error) {
-            return {
-                status: "error",
-                message: e.message
-            };
-        }
-    }
-    return {
-        status: "success",
-        urls: urls
-    };
+        resolve({
+            status: "success",
+            urls: urls
+        });
+    });
 }
