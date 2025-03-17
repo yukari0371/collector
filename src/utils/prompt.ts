c415b638a3139953d7b9eeea6dd767d10f9a8322
歯臭い <tsurumaki0397@icloud.com>
2025-03-17 18:08:50 +0900

    Update prompt.ts

--- src/utils/prompt.ts
+++ src/utils/prompt.ts
@@ -3,6 +3,7 @@ import { colors } from "./logger";
 export async function prompt(question: string): Promise<string> {
     return new Promise(resolve => {
         process.stdout.write(`${colors.magenta}${question}${colors.white}>`);
+                process.stdin.resume();
         process.stdin.once('data', (data) => {
             process.stdin.pause();
             resolve(data.toString().trim());
